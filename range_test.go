e33392b914cf866cb5b97d53a4c66ca091dc66a2
n-ogawa <fy59.kou-729@live.jp>
2022-11-09 23:10:08 +0900

    convert to table drive test

--- range_test.go
+++ range_test.go
@@ -6,18 +6,33 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	targetRange, err := DecideRange("2022-11")
-	if err != nil {
-		panic(err)
+	tests := []struct {
+		name          string
+		targetMonth   string
+		expectedStart time.Time
+		expectedEnd   time.Time
+	}{
+		{
+			name:          time.November.String(),
+			targetMonth:   "2022-11",
+			expectedStart: time.Date(2022, 11, 1, 0, 0, 0, 0, time.UTC),
+			expectedEnd:   time.Date(2022, 11, 30, 0, 0, 0, 0, time.UTC),
+		},
 	}
 
-	expectedStart := time.Date(2022, 11, 1, 0, 0, 0, 0, time.UTC)
-	expectedEnd := time.Date(2022, 11, 30, 0, 0, 0, 0, time.UTC)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			targetRange, err := DecideRange(tt.targetMonth)
+			if err != nil {
+				panic(err)
+			}
 
-	if targetRange.Start != expectedStart {
-		t.Errorf("expected %v, but got %v", expectedStart, targetRange.Start)
-	}
-	if targetRange.End != expectedEnd {
-		t.Errorf("expected %v, but got %v", expectedEnd, targetRange.End)
+			if targetRange.Start != tt.expectedStart {
+				t.Errorf("expected %v, but got %v", tt.expectedStart, targetRange.Start)
+			}
+			if targetRange.End != tt.expectedEnd {
+				t.Errorf("expected %v, but got %v", tt.expectedEnd, targetRange.End)
+			}
+		})
 	}
 }
