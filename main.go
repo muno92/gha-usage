aeda11698337955e676cf18021dedef5dabddb01
n-ogawa <fy59.kou-729@live.jp>
2022-11-21 22:50:13 +0900

    Create main

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github_actions_usage_calculator/runner"
+	"os"
+)
+
+func main() {
+	repo := os.Args[1]
+	targetMonth := os.Args[2]
+	token := os.Getenv("GITHUB_TOKEN")
+
+	usage, err := runner.Run(repo, targetMonth, token)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s %s usage\n", repo, targetMonth)
+	fmt.Printf("Linux: %ds\n", usage.Linux)
+	fmt.Printf("Windows: %ds\n", usage.Windows)
+	fmt.Printf("Mac: %ds\n", usage.Mac)
+}
