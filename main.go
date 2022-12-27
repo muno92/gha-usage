5feeaa2013049870a84fbe01eb7433cf5972a1cd
n-ogawa <fy59.kou-729@live.jp>
2022-12-28 00:22:06 +0900

    Define sum command by flagSet

--- main.go
+++ main.go
@@ -9,20 +9,32 @@ import (
 
 var v bool
 var countCmd *flag.FlagSet
+var sumCmd *flag.FlagSet
 var repo string
 var startDate string
 var endDate string
 
 func init() {
 	countCmd = flag.NewFlagSet("count", flag.ExitOnError)
-	countCmd.StringVar(&repo, "repo", "", "target repository (format: OWNER/REPOSITORY)")
-	countCmd.StringVar(&startDate, "start", "", "start date of count range (format: yyyy-mm-dd)")
-	countCmd.StringVar(&endDate, "end", "", "end date of count range (format: yyyy-mm-dd)")
+	SetFlagOption(countCmd)
+	countCmd.Usage = PrintCountHelp
 
+	sumCmd = flag.NewFlagSet("sum", flag.ExitOnError)
+	SetFlagOption(sumCmd)
+	sumCmd.Usage = PrintSumHelp
+
+	flag.BoolVar(&v, "v", false, "Show Version")
 	flag.BoolVar(&v, "version", false, "Show Version")
 }
 
+func SetFlagOption(flag *flag.FlagSet) {
+	flag.StringVar(&repo, "repo", "", "target repository (format: OWNER/REPO)")
+	flag.StringVar(&startDate, "start", "", "start date of count range (format: yyyy-mm-dd)")
+	flag.StringVar(&endDate, "end", "", "end date of count range (format: yyyy-mm-dd)")
+}
+
 func main() {
+	flag.Usage = PrintHelp
 	flag.Parse()
 	if v {
 		cmd.PrintVersion()
@@ -31,6 +43,11 @@ func main() {
 
 	token := os.Getenv("GITHUB_TOKEN")
 
+	if len(os.Args) == 1 {
+		PrintHelp()
+		os.Exit(1)
+	}
+
 	switch os.Args[1] {
 	case "count":
 		err := countCmd.Parse(os.Args[2:])
@@ -38,8 +55,7 @@ func main() {
 			panic(err)
 		}
 		if repo == "" || startDate == "" || endDate == "" {
-			println("Usage of count:")
-			countCmd.PrintDefaults()
+			PrintCountHelp()
 			os.Exit(1)
 		}
 
@@ -48,15 +64,38 @@ func main() {
 			panic(err)
 		}
 		return
+	case "sum":
+		err := sumCmd.Parse(os.Args[2:])
+		if err != nil {
+			panic(err)
+		}
+		if repo == "" || startDate == "" || endDate == "" {
+			PrintSumHelp()
+			os.Exit(1)
+		}
+
+		err = cmd.SumCommand{Logger: log.Default()}.Run(os.Stdout, repo, startDate, endDate, token)
+		if err != nil {
+			panic(err)
+		}
+		return
 	default:
 	}
+}
 
-	repo := os.Args[1]
-	startDate := os.Args[2]
-	endDate := os.Args[3]
+func PrintCountHelp() {
+	println("Usage of count: ./ghausage count --repo OWNER/REPO --start YYYY-MM-DD --end YYYY-MM-DD")
+	countCmd.PrintDefaults()
+}
 
-	err := cmd.SumCommand{Logger: log.Default()}.Run(os.Stdout, repo, startDate, endDate, token)
-	if err != nil {
-		panic(err)
-	}
+func PrintSumHelp() {
+	println("Usage of sum: ./ghausage sum --repo OWNER/REPO --start YYYY-MM-DD --end YYYY-MM-DD")
+	sumCmd.PrintDefaults()
+}
+
+func PrintHelp() {
+	println("Usage of ./ghausage")
+	println("./ghausage [-v | --version] [-h | --help]")
+	println("./ghausage count    Count GitHub Actions workflow runs of specified repository")
+	println("./ghausage sum      Sum GitHub Actions usage time of specified repository")
 }
